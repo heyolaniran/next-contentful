51278b5f8f7453fc0c5bae874cb2e3cb08c81507
heyolaniran <olaniranlaurent@gmail.com>
2023-03-16 10:56:40 +0100

    feat: contentful dependencies & fetch content model

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import  {createClient} from 'contentful'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getStaticProps () {
+
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE , 
+      accessToken: process.env.CONTENTFUL_ACCESS_KEY , 
+    })
+
+    const res = await client.getEntries({content_type: 'recipe'}) ; 
+
+    return {
+
+      props: {
+        recipes: res.items 
+      }
+
+    }
+
+    return
+}
+
+export default function Home({ recipes }) {
+
+  console.log(recipes)
   return (
     <>
      
