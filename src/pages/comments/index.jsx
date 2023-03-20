7adb61f687e7a4e2d4e37da564188902f40f8f0d
heyolaniran <olaniranlaurent@gmail.com>
2023-03-20 16:16:54 +0100

    feat: POST request

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Comments () {
    
     const [comments, setComments] = useState() ; 
+    const [comment, setComment] = useState('')
+
+    
+    const postComment = async () => {
+        const response =  await fetch("/api/comments", {
+            method: "POST", 
+            body: JSON.stringify({ comment }), 
+            headers : {
+                'Content-Type': 'application/json'
+            }
+        }) ; 
+
+        const data =  await response.json() ; 
+
+        console.log(data)
+    }
 
     const fetchComments = async () => {
         const res = await fetch("http://localhost:3000/api/comments") ; 
@@ -12,6 +28,11 @@ export default function Comments () {
         setComments(data)
 
     }
+
+    useEffect(() => {
+        fetchComments()      
+    }, [comments])
+    
 
    
     return (
@@ -23,13 +44,26 @@ export default function Comments () {
             <div className="grid xs:grid-cols-1 md:grid-cols-3 gap-4">
                 {comments && (
                     comments.map(comment => (
-                        <div className="">
+                        <div className="" key={comment.id}>
                             {comment.text}
                         </div>
                     ))
                 )}
             </div>
             
+
+            <div className="mt-4 justify-center flex">
+                <div className="">
+                    <textarea type="text" className="border border-spacing-1 border-gray-600 rounded-lg grid grid-cols-6" rows={6}  onChange={(e) => {
+                        setComment(e.target.value)
+                    }}>
+                    </textarea>
+
+                    <div className="mt-4">
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-full " onClick={postComment}>Post Comment </button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
